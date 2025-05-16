3eb2996a6a4467736f534e8e07bcfc12e9c1cd97
Saddam <01957987631a@gmail.com>
2025-05-17 01:58:43 +0600

    Test deploy and url

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://159.89.172.251:3086'  // Production API URL
+  baseUrl: 'http://159.89.172.251:3086'  // Production API URL
 };
